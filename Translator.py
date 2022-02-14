055f08c9a79b5251849936e0b964252a54fcd831
Ukeje Chukwuemeriwo Goodness <ukejegoodness599@gmail.com>
2022-02-14 04:58:55 +0100

    Made a mistake

--- Translator.py
+++ Translator.py
@@ -1,7 +1,5 @@
-from translate import Translator
-
-
 def translator(loop_containing_lyrics):
+    translated = ""
     print("You can translate to any of the languages on ISO-639-I")
     print(
         "https://en.wikipedia.org/wiki/ISO_639-1, check this out and input the 2 alpha language code to transalate lyrics")
@@ -10,7 +8,10 @@ def translator(loop_containing_lyrics):
         print("Incorrect code input, should be two letters")
         exit()
     else:
+        
         translator = Translator(to_lang=reqqie)
         for text in loop_containing_lyrics:
             translation = translator.translate(f"{text}")
-    return translation
+            translated += translation
+            
+    return translated
