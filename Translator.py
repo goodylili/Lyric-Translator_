23ce2571eb97bfccb951dae49ae897219d561c70
Ukeje Chukwuemeriwo Goodness <ukejegoodness599@gmail.com>
2022-02-14 04:38:02 +0100

    SEPERATING THE TRANSLATOR FILE
    For better practices on the evolution of my skills, I'm separating the files for better easy of updating them

---
+++ Translator.py
@@ -0,0 +1,19 @@
+from translate import Translator
+
+
+def translator(loop_containing_lyrics):
+    reqqie = input("In what language do you need these lyrics!").lower()
+    if len(reqqie) > 2 or reqqie.isalpha() == False:
+        print("Incorrect code input, should be two letters")
+        exit()
+    else:
+        translator = Translator(to_lang=reqqie)
+        for text in loop_containing_lyrics:
+            translation = translator.translate(f"{text}")
+            print(translation)
+
+
+
+    """print("You can translate to any of the languages on ISO-639-I")
+    print("https://en.wikipedia.org/wiki/ISO_639-1, check this out and input the 2 alpha language code to transalate lyrics")
+    reqqie = input("In what language do you need these lyrics!")"""
