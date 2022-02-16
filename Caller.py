debaba9117b19ee48ca783aa6f7639f2dc5efa25
Ukeje Chukwuemeriwo Goodness <ukejegoodness599@gmail.com>
2022-02-16 02:32:00 +0100

    Update Caller.py

--- Caller.py
+++ Caller.py
@@ -1,1 +1,20 @@
+from Translathor import translator
+from apicalls import *
 
+def caller():
+    intention = input(
+        "To get songs by an artist use (F)\nTo get music lyrics use(G)\nUse (H) to get lyrics and translate -->")
+    if intention.lower() != "g" and intention.lower() != "f" and intention.lower() != "h":
+        exit("Lmao, get serious abeg")
+    elif intention.lower() == "g":
+        for text in get_lyrics():
+            print(text)
+
+    elif intention.lower() == "f":
+        get_songsby()
+
+    else:
+        print(translator(get_lyrics()))
+
+if __name__ == "__main__":
+    print(caller())
